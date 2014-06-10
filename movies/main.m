ddb0f19f227dc282dec2109538388e2c8b00d3e8
piyush123 <piyush@onor.net>
2014-06-10 11:25:27 -0700

    Initial Commit

---
+++ movies/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  movies
+//
+//  Created by piyush shah on 6/10/14.
+//  Copyright (c) 2014 onor inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "moviesAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([moviesAppDelegate class]));
+    }
+}
---
+++ movies/moviesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  moviesAppDelegate.h
+//  movies
+//
+//  Created by piyush shah on 6/10/14.
+//  Copyright (c) 2014 onor inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface moviesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
